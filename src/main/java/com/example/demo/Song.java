7da835f9cde42ea89efc885275a7fff6e917e72b
TPCF235018UR <TPCF235018UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-10-07 16:44:22 -0400

    2.02

---
+++ src/main/java/com/example/demo/Song.java
@@ -0,0 +1,59 @@
+package com.example.demo;
+
+public class Song {
+public long id;
+private String name;
+private String age;
+private String height;
+private int rating;
+private int phonenumber;
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getHeight() {
+        return height;
+    }
+
+    public void setHeight(String height) {
+        this.height = height;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public int getPhonenumber() {
+        return phonenumber;
+    }
+
+    public void setPhonenumber(int phonenumber) {
+        this.phonenumber = phonenumber;
+    }
+}
